ee288e87d22db17d9a3eb48199039f0d2e918684
JorryZ <45359771+JorryZ@users.noreply.github.com>
2020-09-11 17:24:31 +0800

    Update __init__.py

--- myocardiumModel/__init__.py
+++ myocardiumModel/__init__.py
@@ -12,7 +12,9 @@ History:
   Author: jorry.zhengyu@gmail.com         26AUGU2020             -V1.0.1 release version
                                                         -shellModel version 1.0.1 
   Author: jorry.zhengyu@gmail.com         29AUGU2020             -V1.0.2 release version
-                                                        -shellModel version 1.0.2                                        
+                                                        -shellModel version 1.0.2  
+  Author: jorry.zhengyu@gmail.com         05Sept2020             -V2.0.0 test version
+                                                        -shellModel version 2.0.0                                                 
                                                         
 Requirements:
     numpy
@@ -21,7 +23,7 @@ Requirements:
     meshplex
 All rights reserved.
 """
-_version='1.0.2'
+_version='2.0.0'
 print('myocardiumModel version',_version)
 
 import os
